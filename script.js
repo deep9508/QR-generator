53c68471f5973d71a7e53a0941b95293ba019474
DEEPAK KUMAR <112052287+deep9508@users.noreply.github.com>
2023-09-28 20:49:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+function generateQR(){
+    return 
+}
